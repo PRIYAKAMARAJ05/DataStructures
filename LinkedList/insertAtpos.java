86ad470b05c720a79072b7aa74b8a69705c0edfa
PRIYAKAMARAJ05 <107319067+PRIYAKAMARAJ05@users.noreply.github.com>
2023-02-10 19:21:48 +0530

    Create insertAtpos.java

---
+++ LinkedList/insertAtpos.java
@@ -0,0 +1,49 @@
+import java.util.*;
+class Node{
+    int data;
+    Node next;
+}
+class LinkedList{
+    Node head=null;
+    Node createNode(int data){
+        Node nn = new Node();
+        nn.data=data;
+        nn.next=null;
+        return nn;
+    }
+    void insertAtPos(int data,int pos){
+        Node n = createNode(data);
+        Node temp = head;
+        if(head==null){
+            head=n;
+        }
+        else{
+            while(--pos>0){
+                temp=temp.next;
+            }
+            n.next=temp.next;
+            temp.next=n;
+        }
+    }
+    void display(){
+        Node temp = head;
+        while(temp!=null){
+            System.out.println(temp.data+" ");
+            temp=temp.next;
+        }
+    }
+}
+
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		LinkedList l = new LinkedList();
+		l.insertAtPos(10,0);
+		l.insertAtPos(20,1);
+		l.insertAtPos(30,2);
+		l.display();
+	}
+}
